c770b42b585dbc7fe9230e0c83ad3163a0809895
chandra <ck.vadla@gmail.com>
2017-03-03 21:42:55 +0000

    backing up

--- CapstoneProject-test.py
+++ CapstoneProject-test.py
@@ -25,9 +25,12 @@ from sklearn.metrics import accuracy_score
 from sklearn.feature_extraction.text import TfidfTransformer
 import timeit
 
+start_time = timeit.default_timer()
+
+
 
 dataset_filename = '/home/chandra/Prescription-Based-prediction-with-featureSelection/roam_predcription_based_predicition_c.jsonl'
-#dataset_filename ='/home/chandra/Prescription-Based-prediction-with-featureSelection/predBasedPres_corrected_50.jsonl'
+#dataset_filename ='./predBasedPres_corrected_50.jsonl'
 dataframe_main = pd.DataFrame()
 data = ''
 with open(dataset_filename) as json_data:
@@ -37,7 +40,10 @@ json_data.close()
 #print "Finished reding the input file"
 print dataframe_main.shape
 
+elapsed = timeit.default_timer() - start_time
+print elapsed, 'time take to read the dataset'
 
+start_time = timeit.default_timer()
 column_names_list = dataframe_main.columns.values.tolist()
 index_npi = column_names_list.index('npi')
 column_names_list.remove('npi')
@@ -85,28 +91,19 @@ print len(new_names_dict)
 dataframe_main.rename(columns=new_names_dict, inplace=True)
 
 print "dataframe_main shape is:", dataframe_main.shape
-#print "\n\ndataframe_main columns names after renameing:\n ", dataframe_main.columns
+#print "\n\ndataframe_main columns names after renameing:\n ", dataframe_main.columns.values[:-10]
 
 df_specialty_count = pd.DataFrame({'counts' : dataframe_main.groupby( [ "specialty"] , sort=False).size()}).reset_index()
 df_specialty_count.sort_values(['counts'], ascending=False, inplace=True)
-df_specialty_count.head()
-
-print "\n\ndataframe_main columns names after renameing:\n ", dataframe_main.columns
-
-
 
 dataframe_main.drop(['npi', 'brand_name_rx_count', 'gender', 'generic_rx_count', 'region', 'settlement_type', 'years_practicing'], axis=1,inplace=True)
-#dataframe_main.columns.values[-10:]
-
-
 
 #FOR THIS CAPSTONR PROJECT PURPOSE WE WILL CONSIDER ONLY TOP FIVE SPECIALITIES(WHICH CORESPONDS TO 2/3RD'S) FOR THE LACK OF COMPUTATIONAL INFRASTRUCTURE
-
 tempList = []
-df_df_specialty_countlessthan400 = df_specialty_count.query('counts < 5000')
+df_df_specialty_countlessthan400 = df_specialty_count.query('counts < 900')
 for i in df_df_specialty_countlessthan400['specialty']:
     indices = dataframe_main.loc[dataframe_main['specialty'] == i].index.tolist()
-    tempList.append(indices)  
+    tempList.append(indices)
 
 #collect all the indices for these specialties to remove from the df_dataframe_main
 
@@ -116,64 +113,73 @@ dataframe_main.dropna(axis=1,how='all', inplace=True)
 print "Shape of the data is:", dataframe_main.shape
 
 
-
-
-
 label = dataframe_main.pop('specialty')
 #dataframe_main.columns.values[-3:]
 
 dataframe_main.fillna(value=0, inplace=True)
 
+
+elapsed = timeit.default_timer() - start_time
+print elapsed, 'time taken for procressing' 
+
+
+start_time = timeit.default_timer()
+
 tfidf =TfidfTransformer(norm=u'l2', use_idf=True, smooth_idf=True, sublinear_tf=False)
 data =tfidf.fit_transform(dataframe_main.values)
 
-#dataframe_main.head(2)
+elapsed = timeit.default_timer() - start_time
+print elapsed, 'time taken for tfidf'
 
-#type (dataframe_main.values)
 
 
+start_time = timeit.default_timer()
 
 le = LabelEncoder()
 y = le.fit_transform(label)
 
-def featureSelectAndParamTuning(nmf_components):
-	print "trying with NMF n_components=", 50*nmf_components
-	model = NMF(n_components=50*nmf_components, init='random', random_state=0, verbose=0)
-	boost_input = model.fit_transform(data, y=y)
-	#print "Finished NMF"
-	print "Shape of NMF outPut is:", boost_input.shape
-
-	X_train,X_test,y_train,y_test = train_test_split(boost_input,y,test_size=0.2, random_state=42)
-
-	model = XGBClassifier(silent=True,seed=42)
-	param_grid = {
-		#'subsample':[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-                #'scale_pos_weight':[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-                #'colsample_bytree':[0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0],
-                'colsample_bylevel':[0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-
-              }
+elapsed = timeit.default_timer() - start_time
+print elapsed, 'timr take fr label encoder'
 
 
-	start_time = timeit.default_timer()
-	kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+start_time = timeit.default_timer()
+
+nmf = NMF(n_components=50, init='random', random_state=0, verbose=0)
+boost_input = nmf.fit_transform(data, y=y)
+
+elapsed = timeit.default_timer() - start_time
+print elapsed, 'time takken for NMF'
 
 
-	grid_search = GridSearchCV(model, param_grid, n_jobs=-1, cv=kfold)
-	grid_result = grid_search.fit(X_train, y_train)
+X_train,X_test,y_train,y_test = train_test_split(boost_input,y,test_size=0.2, random_state=42)
 
-	# Get the estimator
-	print grid_result.best_estimator_
-	print 'CV Accuracy of best parameters: %3f' %grid_result.best_score_
 
-	model = grid_result.best_estimator_
-	y_pred = model.predict(X_test)
+model = XGBClassifier(silent=True,seed=42, scale_pos_weight=0.1 , nthread=-1)
+param_grid = { 'colsample_bylevel':[0.5,0.6,0.7] }
+kfold = StratifiedKFold(n_splits=3, shuffle=True, random_state=7)
 
-	print "Accuracy Rate, which is calculated by accuracy_score() is: %f" % accuracy_score(y_test, y_pred)
-	#accuracy_score(y_test, y_pred)
-	elapsed = timeit.default_timer() - start_time
-	print elapsed
+start_time = timeit.default_timer()
 
-for i in range(1,11):
-	featureSelectAndParamTuning(i)
+elapsed = timeit.default_timer() - start_time
+grid_search = GridSearchCV(model, param_grid, n_jobs=-1, cv=kfold)
+grid_result = grid_search.fit(X_train, y_train)
+print grid_result.best_estimator_
 
+print elapsed, 'time taken by gridsearchcv.fit'
+
+
+start_time = timeit.default_timer()
+
+model = grid_result.best_estimator_
+y_pred = model.predict(X_test)
+
+elapsed = timeit.default_timer() - start_time
+print elapsed, 'timr take to predict'
+
+
+
+
+print "Accuracy Rate, which is calculated by accuracy_score() is: %f" % accuracy_score(y_test, y_pred)
+
+elapsed = timeit.default_timer() - start_time
+print elapsed
